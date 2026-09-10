c991b813fb27640345a38fa78a518a8fbd98979e
Dogan Demir <Dogandemir2004@gmail.com>
2026-09-10 10:43:57 +0100

    completed workflow

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+const globals = require("globals");
+
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: globals.node,
+    },
+    rules: {
+      "no-unused-vars": "error",
+    },
+  },
+];
