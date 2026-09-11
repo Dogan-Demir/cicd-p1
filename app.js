6e840b623a6125c04fc874f41e75b2fd367f9516
Dogan Demir <Dogandemir2004@gmail.com>
2026-09-11 14:48:39 +0100

    TESTING FULL CI/CD

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from CI/CD!");
+  res.send("Hello from automatic deplyoyment CI/CD!!!!!");
 });
 
 const PORT = process.env.PORT || 3000;
