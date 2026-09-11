c658e00c36f0402c7f6ec6335889c19b807e8f16
Dogan Demir <Dogandemir2004@gmail.com>
2026-09-11 14:55:43 +0100

    Made testing text larger and in center

--- app.js
+++ app.js
@@ -3,7 +3,31 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from automatic deplyoyment CI/CD!!!!!");
+res.send(`
+  <html>
+    <head>
+      <style>
+        body {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+          font-family: Arial, sans-serif;
+        }
+
+        h1 {
+          font-size: 48px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Hello from automatic deployment CI/CD!!!!!</h1>
+    </body>
+  </html>
+`);
+
+
 });
 
 const PORT = process.env.PORT || 3000;
